0061f4a06b9c81134f83747247665bd7e905c1ec
yana <yananasko@gmail.com>
2025-10-01 19:32:43 +0300

    Update main.py

--- task_5/main.py
+++ task_5/main.py
@@ -1,7 +1,9 @@
-number=(input())
-number_str = str(number)
-sum_of_digits = 0
-for digit_char in number_str:
-    digit = int(digit_char)  
-    sum_of_digits += digit 
-    print(f"Сумма цифр числа {number} равна {sum_of_digits}")
+number=(input()) // ввод значения с клавиатуры
+number_str = str(number) // Преобразование числа в строку
+sum_of_digits = 0 // Инициализация переменной для хранения суммы
+//Итерация по символам строки и суммирование цифр
+for digit_char in number_str: 
+    digit = int(digit_char) //Преобразование символа в число  
+    sum_of_digits += digit  //Добавление цифры к сумме
+
+    print(f"Сумма цифр числа {number} равна {sum_of_digits}") // вывод получившегося значения
